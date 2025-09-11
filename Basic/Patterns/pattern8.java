bc402c623d04afbfaaf2a33671dac100990126d5
vishakh k t <vishakhkt.2003@gmail.com>
2025-09-11 20:35:55 +0530

    Day 2 completed

--- Basic/Patterns/pattern8.java
+++ Basic/Patterns/pattern8.java
@@ -1,5 +1,18 @@
 package Patterns;
 
 public class pattern8 {
-    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i >= 1; i--) {   // start from 4 to avoid repeating middle line
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
