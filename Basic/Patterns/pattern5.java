a017c4d3ff44334067debb7d7e4964a1cbac6746
vishakh k t <vishakhkt.2003@gmail.com>
2025-09-11 19:44:47 +0530

    Day 2 1st problem completed

---
+++ Basic/Patterns/pattern5.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<5-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
