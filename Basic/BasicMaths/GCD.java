f26fb63208a907bcde489da3ec7181c0a12559f4
vishakh k t <vishakhkt.2003@gmail.com>
2025-09-16 22:37:18 +0530

    Day 8 completed

---
+++ Basic/BasicMaths/GCD.java
@@ -0,0 +1,21 @@
+package BasicMaths;
+
+public class GCD {
+    public static int findGCD(int a, int b){
+        while(a!=b){
+            if(a>b){
+                a=a-b;
+            }
+            if(b>a){
+                b=b-a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        GCD g= new GCD();
+        int res= g.findGCD(1000, 50);
+        System.out.println(res);
+    }
+}
