9d4ffddffc4f110982413faa57aa1de8b9b1b6eb
vishakh k t <vishakhkt.2003@gmail.com>
2025-09-24 00:05:30 +0530

    valid palindrome is done

--- Basic/BasicMaths/validPalindrome.java
+++ Basic/BasicMaths/validPalindrome.java
@@ -1,5 +1,26 @@
 package BasicMaths;
 
-public  validPalindrome {
+public class validPalindrome {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++i;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                --j;
+            } else if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            } else {
+                ++i;
+                --j;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        validPalindrome v = new validPalindrome();
+        boolean b=v.isPalindrome("hello guys");
+        System.out.println(b);
+    }
     
 }
